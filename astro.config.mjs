3350cdfc2c51c79aa5c1ab133aa0de3b11b990b6
Stefan Rizescu <iulian.stefan.rizescu@gmail.com>
2024-02-13 16:44:48 +0200

    ignore build outputs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-
 import solidJs from "@astrojs/solid-js";
+import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), solidJs()]
+	integrations: [tailwind(), solidJs()],
 });
